e1d13a52c87cc7e6501126b03521bb824e0679e7
zubairkhan1234 <zubairsaleem1210@gmail.com>
2022-08-04 09:11:36 +0500

    created login and signUp page using bootstrap 5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
